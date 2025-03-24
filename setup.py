2ef190bcec85a45f3058230c3d84d721d5f0fd11
pks98 <prajwalks98@gmail.com>
2025-03-25 00:43:27 +0530

    folder and files setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MedicalChatBot Project",
+    version="0.0.0",
+    author="Prajwal",
+    packages=find_packages(),
+    install_requires = []
+)
